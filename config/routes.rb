93dd8a5aba0495c4f50363b7e896019b648a00de
AkaneU <akane0419.u0615@gmail.com>
2021-12-12 03:15:14 +0000

    [Change] sessions/new.html.erb and registrations/new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :customers,skip: [:passwords,], controllers: {
+  devise_for :end_users,skip: [:passwords,], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
+
+  root :to => "public/homes#top"
 end
