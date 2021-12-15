750cd1fca50e5362f8f6ba57708a33fd75d67600
AkaneU <akane0419.u0615@gmail.com>
2021-12-15 14:04:53 +0000

    [Edit] controller, model, and view

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,8 @@ class Post < ApplicationRecord
   accepts_attachments_for :image_files, attachment: :image, append: true
 
   acts_as_taggable
+
+  def favorited_by?(end_user)
+    favorites.where(end_user_id: end_user.id).exists?
+  end
 end
