1508a0b44567a862d1b25e725329e3bab85b1121
AkaneU <akane0419.u0615@gmail.com>
2021-12-16 08:41:05 +0000

    [Edit] controller, routes, and views

--- app/controllers/public/inquiries_controller.rb
+++ app/controllers/public/inquiries_controller.rb
@@ -1,7 +1,28 @@
 class Public::InquiriesController < ApplicationController
+
   def new
+    @inquiry = Inquiry.new
   end
 
   def confirm
+    @inquiry = Inquiry.new(inquiry_params)
   end
+
+  def create
+    @inquiry = Inquiry.new(inquiry_params)
+    if params[:back]
+      render :new
+      return
+    else
+      @inquiry.save
+      redirect_to end_user_path(current_end_user.id)
+    end
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :title, :text)
+  end
+
 end
