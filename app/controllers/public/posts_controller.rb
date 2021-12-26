bdf30e53b3928db661ee0cb09ad71d54f461f598
AkaneU <akane0419.u0615@gmail.com>
2021-12-26 17:23:54 +0000

    [Edit] fix errors

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -17,24 +17,31 @@ class Public::PostsController < ApplicationController
   end
 
   def this_week_popular
-    @posts = Post.joins(:favorites).where(favorites: {created_at: Time.current.all_week}).group(:post_id).order(favorited_end_users: :desc).limit(50).page(params[:page]).per(10)
-    @posts.each do |post|
-      post = Post.includes(:image_files)
-    end
+    posts = Post.find(Favorite.group(:post_id).where(favorites: {created_at: Time.current.all_week}).group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    @posts = Kaminari.paginate_array(posts).limit(50).page(params[:page]).per(10)
+
+    #@posts = Post.joins(:favorites).where(favorites: {created_at: Time.current.all_week}).group(:post_id).order(favorited_end_users: :desc).limit(50).page(params[:page]).per(10)
+    #@posts.each do |post|
+    #  post = Post.includes(:image_files)
+    #end
   end
 
   def this_month_popular
-    @posts = Post.joins(:favorites).where(favorites: {created_at: Time.current.all_month}).group(:post_id).order(favorited_end_users: :desc).limit(50).page(params[:page]).per(10)
-    @posts.each do |post|
-      post = Post.includes(:image_files)
-    end
+    posts = Post.find(Favorite.group(:post_id).where(favorites: {created_at: Time.current.all_month}).group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    @posts = Kaminari.paginate_array(posts).limit(50).page(params[:page]).per(10)
+    #@posts = Post.joins(:favorites).where(favorites: {created_at: Time.current.all_month}).group(:post_id).order(favorited_end_users: :desc).limit(50).page(params[:page]).per(10)
+    #@posts.each do |post|
+    #  post = Post.includes(:image_files)
+    #end
   end
 
   def last_month_popular
-    @posts = Post.joins(:favorites).where(favorites: {created_at: Time.current.last_month.all_month}).group(:post_id).order(favorited_end_users: :desc).limit(50).page(params[:page]).per(10)
-    @posts.each do |post|
-      post = Post.includes(:image_files)
-    end
+    posts = Post.find(Favorite.group(:post_id).where(favorites: {created_at: Time.current.last_month.all_month}).group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    @posts = Kaminari.paginate_array(posts).limit(50).page(params[:page]).per(10)
+    #@posts = Post.joins(:favorites).where(favorites: {created_at: Time.current.last_month.all_month}).group(:post_id).order(favorited_end_users: :desc).limit(50).page(params[:page]).per(10)
+    #@posts.each do |post|
+    #  post = Post.includes(:image_files)
+    #end
   end
 
   def favorited
