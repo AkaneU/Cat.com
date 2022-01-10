ec3a4a2e633b0f29c42b34e999e06b6662bfd0dd
AkaneU <akane0419.u0615@gmail.com>
2022-01-10 14:45:51 +0000

    [Add] comments

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,5 +1,6 @@
 class Public::SearchesController < ApplicationController
-
+  
+  #検索ワードの画面にて人気のタグを10個表示
   def search
     @tags = Post.tag_counts_on(:tags).most_used(10)
   end
@@ -9,7 +10,8 @@ class Public::SearchesController < ApplicationController
     @content = params["content"]
     @records = search_for(@model, @content).page(params[:page]).per(10)
   end
-
+  
+  #全てのタグを表示
   def all_tags
     @tags = Post.tag_counts_on(:tags)
   end
