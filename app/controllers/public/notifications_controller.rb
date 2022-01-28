4c2f556dd7bc02f1886969062c0281ef26dea486
AkaneU <akane0419.u0615@gmail.com>
2022-01-28 14:46:20 +0000

    [Edit]

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,6 +1,7 @@
 class Public::NotificationsController < ApplicationController
   def index
     @notifications = current_end_user.passive_notifications.order(created_at: :desc).page(params[:page]).per(15)
+    #未読の通知をindexアクションを行うことで既読に変更する
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
