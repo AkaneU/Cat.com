609f5b0c092909726836fc7b4d1f6f33726cc7d7
AkaneU <akane0419.u0615@gmail.com>
2022-02-03 15:00:00 +0000

    [Edit]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  #ログインを行っていない場合、トップページを以外のアクセスを制限
   before_action :authenticate_end_user!, except: [:top], if: :public_url
+  #管理者としてログインしていない時、管理者機能のページへのアクセスを制限
   before_action :authenticate_admin!, if: :admin_url
 
   def public_url
